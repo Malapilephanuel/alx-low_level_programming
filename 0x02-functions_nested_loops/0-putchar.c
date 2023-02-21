532b3386d70571c8b41bc4250d1f16bd80ffda3e
Malapilephanuel <Malapilephanuel@gmail.com>
2023-02-21 06:33:24 -0800

    _putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+
+	_putchar('_');
+	_putchar('t');
+	_putchar('h');
+	_putchar('p');
+	_putchar('r');
+	_putchar('u');
+	_putchar('c');
+	_putchar('a');
+	_putchar('\n');
+	return (0);
+
+}
